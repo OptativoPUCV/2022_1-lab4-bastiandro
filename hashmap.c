28abc7b0a805a9bc31ee4e74a4da9b4b2edc7c94
bastiandro <>
2022-04-09 22:41:42 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -88,7 +88,7 @@ Pair * searchMap(HashMap * map,  char * key) {
   while(uno != NULL){
     if(is_equal(map->buckets[aux]->key,key)==1){
       return uno->value;
-      map->current=uno->value;
+      map->current=uno;
     }
     uno = uno +1;
     
