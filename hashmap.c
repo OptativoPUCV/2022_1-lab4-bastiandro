148a77d7a909142a254d2e63e0cc1920fe538e96
bastiandro <>
2022-04-10 00:00:11 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -86,12 +86,12 @@ Pair * searchMap(HashMap * map,  char * key) {
   long aux=hash(key,map->capacity);
   Pair*uno=map->buckets[aux];
   while(uno != NULL){
-    if(is_equal(map->buckets[aux]->key,key)==0){
+    if(is_equal(map->buckets[aux]->key,key)==1){
       return uno->value; 
     }
     uno = uno +1;
   }
-    map->current=*Pair->uno;
+    map->current->key=uno;
     return NULL;
 }
 
