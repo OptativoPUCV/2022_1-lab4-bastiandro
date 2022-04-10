890913db49dad3514a437005eaa3115f0417f8d6
bastiandro <>
2022-04-10 20:04:59 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -82,20 +82,7 @@ void eraseMap(HashMap * map,  char * key) {
 }
 
 Pair * searchMap(HashMap * map,  char * key) { 
-  long aux=hash(key,map->capacity);
-  Pair*i=map->buckets[aux];
   
-  while(i != NULL || (is_equal(map->buckets[aux]->key,key)==1) ){
-      return  (i->value);
-    
-      map->current=i;
-    }
-  if (i== NULL || i->key==NULL){
-    return NULL;
-    }
-    i = i +1;
-    
-    return i;
   }
 
 Pair * firstMap(HashMap * map) {
