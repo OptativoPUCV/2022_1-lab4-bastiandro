ce8d8cfe70eaf1aaadbbb640bf55cdb73fb20cd3
bastiandro <>
2022-04-11 00:40:57 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -80,7 +80,7 @@ HashMap * createMap(long capacity) {
 void eraseMap(HashMap * map,  char * key) {  
   long pos=hash(key,map->capacity);
   Pair*i=map->buckets[pos];
-   if (map->buckets[aux]== NULL || i->key==NULL){
+   if (map->buckets[pos]== NULL || i->key==NULL){
     return NULL;
     }
   while(is_equal(key,map->buckets[pos]->key)==0 ){
